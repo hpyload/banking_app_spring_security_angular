958e0ba6214b8e80e03867ee7ddc627fa8be0b77
Nabil ML <n.oumellahni@gmail.com>
2023-09-30 18:55:02 +0100

    initializer

---
+++ src/main/java/com/springboot_banking/SpringbootBankingApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBankingApplication.class, args);
+    }
+
+}
